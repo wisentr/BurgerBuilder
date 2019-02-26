8091eb2f902fb06119470e06398c6ce66ed249e4
vai91 <mehmetefeekiner@gmail.com>
2019-02-26 10:36:14 +0100

    Add Ingredient component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, BackDrop</div>
-        <main>
+        <main className={classes.Content}>
             {/*Component we wrap with this layout.. */}
             {props.children}
         </main>
