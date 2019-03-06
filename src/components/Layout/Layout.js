f35570b825a47f4a7cb78eabdf7fd0d4a5294e6d
vai91 <mehmetefeekiner@gmail.com>
2019-03-06 11:56:44 +0100

    Start Burger component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,7 +6,15 @@ const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, BackDrop</div>
         <main className={classes.Content}>
-            {/*Component we wrap with this layout.. */}
+            {/*Component we wrap with this layout.. 
+            About props.children -->
+            The React docs say that you can use props.children on 
+        components that represent ‘generic boxes’ and that ‘don’t 
+        know their children ahead of time’.
+            Whenever this component(Layout) is invoked 
+        {props.children} will also be displayed and this is 
+        just a reference to what is between the opening and 
+        closing tags of the component.*/}
             {props.children}
         </main>
     </Aux>
