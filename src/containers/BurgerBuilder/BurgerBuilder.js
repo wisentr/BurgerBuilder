a29150e9b25551d6de6e5b159ed5c3fc4e7ad734
vai91 <mehmetefeekiner@gmail.com>
2019-03-11 20:29:14 +0100

    Add Order button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,9 +18,21 @@ class BurgerBuilder extends Component {
             cheese: 0,
             meat: 0
         },
-        totalPrice: 4
+        totalPrice: 4,
+        purchaseable: false
     }
     
+    updatePurchaseState(ingredients) {
+        const sum = Object.keys(ingredients)
+            .map(igKey => {
+                return ingredients[igKey];
+            })
+            .reduce((acc, currVal) => {
+                return acc + currVal;
+            }, 0);
+        this.setState({purchaseable: sum > 0});
+    }
+
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
@@ -34,6 +46,7 @@ class BurgerBuilder extends Component {
         this.setState({
             totalPrice: newPrice, ingredients: updatedIngredients
         });
+        this.updatePurchaseState(updatedIngredients);
     }
 
     removeIngredientHandler = (type) => {
@@ -52,6 +65,7 @@ class BurgerBuilder extends Component {
         this.setState({
             totalPrice: newPrice, ingredients: updatedIngredients
         });
+        this.updatePurchaseState(updatedIngredients);
     }
     render(){
         /*update the copied object, and store boolean instead of numbers, to basically 
@@ -67,6 +81,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
+                    purchaseable={this.state.purchaseable}
                     price={this.state.totalPrice}/>
             </Aux>
         );
