82ff651b50d904d23d621b13bb7b5daaca25de64
Efe <mehmetefeekiner@gmail.com>
2019-08-25 16:14:48 +0200

    Add Side Drawer, and style it to look good on mobile devices

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
     <Aux>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {/*Component we wrap with this layout.. 
                 About props.children -->
